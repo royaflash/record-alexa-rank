64e26ee48f29dd8c1f6c6481d3eea7fe0b638713
root <root@ip-172-31-34-72.eu-west-1.compute.internal>
2016-08-16 23:32:35 +0430

    bugfix in sed delimeter

--- setup.sh
+++ setup.sh
@@ -14,7 +14,7 @@ virtualhost_dir="$virtualhost_dir/" ; fi
 # Replace domain and dir in source code
 sed -i -- "s/DOMAIN/$domain/g" src/script.php
 sed -i -- "s/DOMAIN/$domain/g" src/domain.php
-sed -i -- "s/DIR/$virtualhost_dir/g" src/domain.php
+sed -i -- "s#DIR#$virtualhost_dir#g" src/domain.php
 
 # Copy and install the script in the right place.
 cp src/script.php /opt/alexa-script.php
