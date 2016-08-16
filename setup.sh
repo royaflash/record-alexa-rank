7f9d97101a997633f22be36f91457452a8e066b1
root <root@ip-172-31-34-72.eu-west-1.compute.internal>
2016-08-16 22:43:36 +0430

    bugfix

--- setup.sh
+++ setup.sh
@@ -12,9 +12,9 @@ if [[ "$virtualhost_dir" != *\/ ]]; then
 virtualhost_dir="$virtualhost_dir/" ; fi
 
 # Replace domain and dir in source code
-sed -i -- 's/DOMAIN/$domain/g' src/script.php
-sed -i -- 's/DOMAIN/$domain/g' src/domain.php
-sed -i -- 's/DIR/$virtualhost_dir/g' src/domain.php
+sed -i -- "s/DOMAIN/$domain/g" src/script.php
+sed -i -- "s/DOMAIN/$domain/g" src/domain.php
+sed -i -- "s/DIR/$virtualhost_dir/g" src/domain.php
 
 # Copy and install the script in the right place.
 cp src/script.php /opt/alexa-script.php
