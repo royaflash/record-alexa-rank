6d966c8ab29f73baf7fffad83b7dd9b1e0c38af0
root <root@ip-172-31-34-72.eu-west-1.compute.internal>
2016-08-16 23:41:05 +0430

    dir string in script.php

--- setup.sh
+++ setup.sh
@@ -15,6 +15,7 @@ virtualhost_dir="$virtualhost_dir/" ; fi
 sed -i -- "s/DOMAIN/$domain/g" src/script.php
 sed -i -- "s/DOMAIN/$domain/g" src/domain.php
 sed -i -- "s#DIR#$virtualhost_dir#g" src/domain.php
+sed -i -- "s#DIR#$virtualhost_dir#g" src/script.php
 
 # Copy and install the script in the right place.
 cp src/script.php /opt/alexa-script.php
